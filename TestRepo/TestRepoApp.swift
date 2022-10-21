1829951e24f92b02bd385e2a07901522fd329d1e
rolandem <rolande.millot@hotmail.com>
2022-10-21 11:53:24 +0200

    Initial Commit

---
+++ TestRepo/TestRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestRepoApp.swift
+//  TestRepo
+//
+//  Created by rolande on 21/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
